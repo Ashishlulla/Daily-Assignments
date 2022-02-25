a2b23fd618b785713017d122325b8b29cc18ba0e
Ashish Rajesh Lulla <lullaashish2807@gmail.com>
2022-02-25 20:07:47 +0530

    Create codes6.py

---
+++ Wipro_codes/codes6.py
@@ -0,0 +1,23 @@
+a = input()
+new_string = ""
+vowels = "AEIOUaeiou"
+lower_vowels = ["a", "e", "i", "o", "u"]
+upper_vowels = ["A", "E", "I", "O", "U"]
+for i in a:
+    if i in vowels:
+        if i.islower():
+            if i == "u":
+                new_string+="a"
+            else:
+                index_i = lower_vowels.index(i)
+                new_string+=lower_vowels[index_i+1]
+        else:
+            if i == "U":
+                new_string+="A"
+            else:
+                index_i = lower_vowels.index(i)
+                new_string+=lower_vowels[index_i+1]
+    else:
+        new_string+=i
+
+print(new_string)
